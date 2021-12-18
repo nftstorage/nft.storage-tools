ccbf54d0653a2082056ddf60115bf7ee5df768fc
redaphid <iam@aaronherres.com>
2021-12-17 17:46:56 -0700

    base case return array

--- test/directory-upload.test.js
+++ test/directory-upload.test.js
@@ -1,3 +1,4 @@
+import {mkdirSync} from "fs"
 import { directoryUpload } from "../lib/directory-upload.js"
 describe("Directory Uploading", () => {
   describe("directoryUpload", () => {
@@ -5,6 +6,13 @@ describe("Directory Uploading", () => {
       expect(directoryUpload).toBeDefined()
     })
     describe("given a directory with no files", () => {
+      beforeEach(() => {
+        try {
+          //Unfortunately, you can't commit an empty directory to git.
+          mkdirSync("./test/data/empty-directory")
+        }
+        catch(e){}
+      })
       it("should return an empty array", async () => {
         const results = await directoryUpload("./test/data/empty-directory")
         expect(results).toEqual([])
