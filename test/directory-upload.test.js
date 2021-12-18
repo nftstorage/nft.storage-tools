182b9f32f5f54be1e20e007cc5baa66565d00b39
redaphid <iam@aaronherres.com>
2021-12-17 17:40:32 -0700

    base case empty dir test

--- test/directory-upload.test.js
+++ test/directory-upload.test.js
@@ -1,8 +1,14 @@
 import { directoryUpload } from "../lib/directory-upload.js"
-describe('Directory Uploading', () => {
-  describe('directoryUpload', () => {
+describe("Directory Uploading", () => {
+  describe("directoryUpload", () => {
     it("should exist", () => {
       expect(directoryUpload).toBeDefined()
+    })
+    describe("given a directory with no files", () => {
+      it("should return an empty array", async () => {
+        const results = await directoryUpload("./test/data/empty-directory")
+        expect(results).toEqual([])
+      })
     })
   })
 })
