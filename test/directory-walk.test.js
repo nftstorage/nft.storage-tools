308ad874b705e166db6ab1612fd59fd860b2e2a4
redaphid <iam@aaronherres.com>
2022-01-06 16:55:49 -0700

    helper util to traverse the ipfs folder structure

--- test/directory-walk.test.js
+++ test/directory-walk.test.js
@@ -1,6 +1,6 @@
 import { getIpfsDirectoryInfo } from "../lib/directory-upload.js"
 import { create as ipfsHttpCreate } from "ipfs-http-client"
-
+import { map, path, compose, keys } from "ramda"
 
 // Note: to avoid rate limiting, consider running a local ipfs daemon and running the tests like this:
 // IPFS_URL="http://localhost:5001/api/v0" npm run test 
@@ -11,6 +11,14 @@ const IPFS_URL = process.env.IPFS_URL || "https://dweb.link/api/v0"
 const getFileCount = (dir) => {
   return Object.keys(dir).length
 }
+
+const getFiles = map(
+  path(["files"])
+)
+const ls  = (f)=>{
+  return map(ls, getFiles(f))  
+}
+
 describe("Directory Walking in IPFS", () => {
   describe("directoryWalk", () => {
     it("should exist", () => {
@@ -19,7 +27,7 @@ describe("Directory Walking in IPFS", () => {
   })
   describe("given an ipfs client", () => {
     let ipfs
-    beforeAll(async() => {
+    beforeAll(async () => {
       ipfs = ipfsHttpCreate(IPFS_URL)
       await ipfs.id
     })
@@ -29,7 +37,8 @@ describe("Directory Walking in IPFS", () => {
       beforeAll(async () => {
         const node_modules_cid = "bafybeidl7ozkgaya4jb6tt3ey5t7pw7uefjfdirwfnnplrim2ksw7a4doi"
         dirinfo = await getIpfsDirectoryInfo(node_modules_cid, ipfs)
-        console.log(JSON.stringify(dirinfo, null, 2))
+        // console.log(JSON.stringify(dirinfo, null, 2))
+        console.log(JSON.stringify(ls(dirinfo),null, 2))
       })
 
       it("should return an array with 1 folder", () => {
