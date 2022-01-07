0b62e696ed749bba25d6180be53635469700b686
redaphid <iam@aaronherres.com>
2022-01-06 21:41:17 -0700

    retry with backoff, jitter, and telling you what it's doing

--- upload-as-car-streaming.js
+++ upload-as-car-streaming.js
@@ -8,30 +8,44 @@ import { readFile } from "fs/promises"
 import dotenv from "dotenv"
 import { NFTStorage, File } from "nft.storage"
 import recursive from "recursive-readdir"
-import { Semaphore } from 'await-semaphore'
+
+import { Semaphore } from "await-semaphore"
+
+import { promisify } from "util"
+const timeout = promisify(setTimeout)
 
 const storeFiles = async ({ endpoint, token, path, maxConcurrentUploads }) => {
-  const image =  new File(await readFile(process.cwd() + "/status.js"), "status.js", {type:"image/jpg"})
+  const image = new File(await readFile(process.cwd() + "/status.js"), "status.js", { type: "image/jpg" })
   const limiter = new Semaphore(maxConcurrentUploads)
 
   const client = new NFTStorage({ endpoint, token })
+
   const files = await recursive(path)
+
   for (const file of files) {
-    const release = await limiter.acquire()
-    try {
-      client.store({
-        name: file,
-        image,
-        description: "aaron's fancy uploader",
-        properties: {
-          file: new File(await readFile(file), file)
-        }
-      })
-    } catch (e) {
-      console.error(e)
-    } finally {
-      release()
+    const release = await limiter.acquire()    
+    const fileProps = {
+      name: file,
+      image,
+      description: "aaron's fancy uploader",
+      properties: {
+        file: new File(await readFile(file), file)
+      }
     }
+    retryClientStore(client, fileProps).then(console.log).finally(release)
+  }
+}
+
+const retryClientStore = async (client, fileProps, timeToWait=500) => {
+  console.log(`uploading ${fileProps.name}...`)
+  try {
+    return await client.store(fileProps)
+  } catch (e) {
+    console.log(`error uploading ${fileProps.name}: ${e.message}`)  
+    timeToWait *= 1.2 + Math.random() // add some jitter
+    console.log("will retry in", timeToWait, "ms")
+    await timeout(timeToWait)
+    return retryClientStore(client, fileProps, timeToWait)
   }
 }
 
@@ -50,7 +64,7 @@ async function main() {
   }
 
   const endpoint = process.env.ENDPOINT || "https://api.nft.storage"
-  const maxConcurrentUploads = process.env.MAX_CONCURRENT_UPLOADS || 5
+  const maxConcurrentUploads = process.env.MAX_CONCURRENT_UPLOADS || 3
   await storeFiles({ endpoint, token, path: filePath, maxConcurrentUploads })
 }
 
