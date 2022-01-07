3157b1c63ef28b0007e3395f6c01468e0568ba6f
redaphid <iam@aaronherres.com>
2022-01-06 22:02:39 -0700

    try to force it to text/plain so I see something

--- upload-directory.js
+++ upload-directory.js
@@ -23,25 +23,25 @@ const storeFiles = async ({ endpoint, token, path, maxConcurrentUploads }) => {
   const files = await recursive(path)
 
   for (const file of files) {
-    const release = await limiter.acquire()    
+    const release = await limiter.acquire()
     const fileProps = {
       name: file,
       image,
       description: "aaron's fancy uploader",
       properties: {
-        file: new File(await readFile(file), file)
+        file: new File(await readFile(file), file, { type: "text/plain" })
       }
     }
     retryClientStore(client, fileProps).then(console.log).finally(release)
   }
 }
 
-const retryClientStore = async (client, fileProps, timeToWait=500) => {
+const retryClientStore = async (client, fileProps, timeToWait = 500) => {
   console.log(`uploading ${fileProps.name}...`)
   try {
     return await client.store(fileProps)
   } catch (e) {
-    console.log(`error uploading ${fileProps.name}: ${e.message}`)  
+    console.log(`error uploading ${fileProps.name}: ${e.message}`)
     timeToWait *= 1.2 + Math.random() // add some jitter
     console.log("will retry in", timeToWait, "ms")
     await timeout(timeToWait)
