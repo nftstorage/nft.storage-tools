cb6e89d023988e375267d308425b862a912b8b43
redaphid <iam@aaronherres.com>
2022-01-06 20:52:35 -0700

    working on parallelizing file uploads

--- upload-as-car-streaming.js
+++ upload-as-car-streaming.js
@@ -4,56 +4,35 @@
  * Usage:
  *     node upload-as-car.js path/to/file
  */
-import { PassThrough } from "stream"
+import { readFile } from "fs/promises"
 import dotenv from "dotenv"
-import fetch from "node-fetch"
-import { NFTStorage } from "nft.storage"
+import { NFTStorage, File } from "nft.storage"
+import recursive from "recursive-readdir"
+import { Semaphore } from 'await-semaphore'
 
-import { packToStream } from "ipfs-car/pack/stream"
-import { FsBlockStore } from "ipfs-car/blockstore/fs"
+const storeFiles = async ({ endpoint, token, path, maxConcurrentUploads }) => {
+  const image =  new File(await readFile(process.cwd() + "/status.js"), "status.js", {type:"image/jpg"})
+  const limiter = new Semaphore(maxConcurrentUploads)
 
-
-const storeStream = async ({ endpoint, token, path,chunkSize }) => {
-  const url = new URL("upload/", endpoint)
-  const stream = new PassThrough()
-  
-  const sendIt = async (buffer) => {
-    console.log("sending", buffer.length)
-    const response = await fetch(url.toString(), {
-      method: "POST",
-      headers: NFTStorage.auth(token),
-      body: buffer,
-    })
-    const result = await response.json()
-    console.log("sendIt", result)
-  }
-
-  packToStream({
-    input: path,
-    writable: stream,
-    blockstore: new FsBlockStore()
-  })
-  let buffer = []
-  
-  for await (const chunk of stream) {
-    buffer = [...buffer, ...chunk]
-    if(buffer.length > chunkSize) {
-      sendIt(buffer)
-      buffer = []
+  const client = new NFTStorage({ endpoint, token })
+  const files = await recursive(path)
+  for (const file of files) {
+    const release = await limiter.acquire()
+    try {
+      client.store({
+        name: file,
+        image,
+        description: "aaron's fancy uploader",
+        properties: {
+          file: new File(await readFile(file), file)
+        }
+      })
+    } catch (e) {
+      console.error(e)
+    } finally {
+      release()
     }
   }
-
-  sendIt(buffer)
-
-
-  
-  console.log(await response.json())
-  // console.log(`read: ${stream.bytesRead} bytes`)
-  // if (result.ok) {
-  //   return result.value.cid
-  // } else {
-  //   throw new Error(result.error.message)
-  // }
 }
 
 
@@ -71,8 +50,8 @@ async function main() {
   }
 
   const endpoint = process.env.ENDPOINT || "https://api.nft.storage"
-  const chunkSize = process.env.CHUNK_SIZE || 1024 * 1024 * 10
-  await storeStream({ endpoint, token, path: filePath, chunkSize })
+  const maxConcurrentUploads = process.env.MAX_CONCURRENT_UPLOADS || 5
+  await storeFiles({ endpoint, token, path: filePath, maxConcurrentUploads })
 }
 
 main()
