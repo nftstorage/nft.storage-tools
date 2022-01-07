09291cad75e814d67fa251263a5bc282c1edb9c9
redaphid <iam@aaronherres.com>
2022-01-06 20:19:40 -0700

    save the streaming impl before I upload the files manually

--- upload-as-car-streaming.js
+++ upload-as-car-streaming.js
@@ -13,31 +13,47 @@ import { packToStream } from "ipfs-car/pack/stream"
 import { FsBlockStore } from "ipfs-car/blockstore/fs"
 
 
-const storeStream = async ({ endpoint, token, path }) => {
+const storeStream = async ({ endpoint, token, path,chunkSize }) => {
   const url = new URL("upload/", endpoint)
   const stream = new PassThrough()
-
-  const fetchPromise = fetch(url.toString(), {
-    method: "POST",
-    headers: NFTStorage.auth(token),
-    body: stream,
-  })
+  
+  const sendIt = async (buffer) => {
+    console.log("sending", buffer.length)
+    const response = await fetch(url.toString(), {
+      method: "POST",
+      headers: NFTStorage.auth(token),
+      body: buffer,
+    })
+    const result = await response.json()
+    console.log("sendIt", result)
+  }
 
   packToStream({
     input: path,
     writable: stream,
     blockstore: new FsBlockStore()
   })
-
-
-  const result = await fetchPromise
-  console.log(JSON.stringify(result))
-  console.log(`read: ${stream.bytesRead} bytes`)
-  if (result.ok) {
-    return result.value.cid
-  } else {
-    throw new Error(result.error.message)
+  let buffer = []
+  
+  for await (const chunk of stream) {
+    buffer = [...buffer, ...chunk]
+    if(buffer.length > chunkSize) {
+      sendIt(buffer)
+      buffer = []
+    }
   }
+
+  sendIt(buffer)
+
+
+  
+  console.log(await response.json())
+  // console.log(`read: ${stream.bytesRead} bytes`)
+  // if (result.ok) {
+  //   return result.value.cid
+  // } else {
+  //   throw new Error(result.error.message)
+  // }
 }
 
 
@@ -55,8 +71,8 @@ async function main() {
   }
 
   const endpoint = process.env.ENDPOINT || "https://api.nft.storage"
-
-  await storeStream({ endpoint, token, path: filePath })
+  const chunkSize = process.env.CHUNK_SIZE || 1024 * 1024 * 10
+  await storeStream({ endpoint, token, path: filePath, chunkSize })
 }
 
 main()
